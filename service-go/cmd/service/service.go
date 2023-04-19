99dd9fb83117e7851c6482d3c863833c4d0a511b
Alla Petrova <allaptr@gmail.com>
2023-04-18 17:17:29 -0700

    add jwt validation

--- service-go/cmd/service/service.go
+++ service-go/cmd/service/service.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	jwtValidator "interview-service/internal/domain/jwt"
+	"log"
 	"net"
 
 	log "github.com/sirupsen/logrus"
@@ -52,8 +58,31 @@ func start() {
 	}
 
 	var opts []grpc.ServerOption
+	{
+		grpc.UnaryInterceptor(grpc_auth.UnaryServerInterceptor(AuthFunc([]byte("secret"))))
+	}
+
 	grpcServer := grpc.NewServer(opts...)
 	interview.RegisterInterviewServiceServer(grpcServer, api.New())
 	reflection.Register(grpcServer)
 	grpcServer.Serve(lis)
+}
+
+const authHeader = "authorization"
+
+func AuthFunc(secret []byte) func(ctx context.Context) (context.Context, error) {
+	return func(ctx context.Context) (context.Context, error) {
+		token, err := grpc_auth.AuthFromMD(ctx, "bearer")
+		if err != nil {
+			return nil, err
+		}
+
+		claims, err := jwtValidator.ValidateToken(token, secret)
+		if err != nil {
+			log.Default().Println(err)
+			return nil, status.Errorf(codes.Unauthenticated, "invalid auth token: %v", err)
+		}
+		ctx = context.WithValue(ctx, authHeader, claims)
+		return ctx, nil
+	}
 }
