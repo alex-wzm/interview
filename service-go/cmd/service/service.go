b0571573ce5db5fd8fe6dd2673f13f2f43987977
Alla Petrova <allaptr@gmail.com>
2023-04-18 17:12:35 -0700

    Still merging

--- service-go/cmd/service/service.go
+++ service-go/cmd/service/service.go
@@ -2,44 +2,23 @@ package main
 
 import (
 	"context"
-<<<<<<< HEAD
 	"fmt"
 	"net"
 	"os"
 
-	config "interview-service/config"
-=======
 	"flag"
-	"os"
-
-	"fmt"
->>>>>>> cdec08f (merged upstream changes)
+	config "interview-service/config"
 
 	log "github.com/sirupsen/logrus"
-
-	"net"
 
 	"interview-service/internal/api"
 	"interview-service/internal/api/interview"
 	jwt "interview-service/internal/domain/jwt"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
-=======
-=======
->>>>>>> cdec08f (merged upstream changes)
-	"flag"
-	"os"
 
 	"github.com/joho/godotenv"
-<<<<<<< HEAD
->>>>>>> d07b04f (Containerized server)
-=======
-=======
-	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
->>>>>>> merge-up
->>>>>>> cdec08f (merged upstream changes)
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
@@ -50,9 +29,6 @@ func main() {
 	loglevel := flag.Int("logLevel", 4, "Useful Log levels: Warn = 3; Info = 4; Debug = 5;")
 	logGrpc := flag.Bool("logGrpc", false, "Turn ON/OFF grpc middleware logs")
 	flag.Parse()
-
-	grpcConfig := config.LoadConfigFromFile(configPath)
-	address := fmt.Sprintf("%s:%s", grpcConfig.ServerHost, grpcConfig.UnsecurePort)
 
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
@@ -71,11 +47,13 @@ func main() {
 		log.Debugf("Verbosity: %s", os.Getenv("GRPC_GO_LOG_VERBOSITY_LEVEL"))
 	}
 
-	start()
+	grpcConfig := config.LoadConfigFromFile(configPath)
+	address := fmt.Sprintf("%s:%s", grpcConfig.ServerHost, grpcConfig.UnsecurePort)
+
+	start(address)
 }
 
-func start() {
-	address := fmt.Sprintf("localhost:%d", 8080)
+func start(address string) {
 	log.WithFields(log.Fields{"address": address}).Info("Starting interview service")
 
 	lis, err := net.Listen("tcp", address)
@@ -83,7 +61,6 @@ func start() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-<<<<<<< HEAD
 	var jwtSecret = os.Getenv("JWT_SECRET")
 
 	if jwtSecret == "" {
@@ -93,11 +70,6 @@ func start() {
 	opts := []grpc.ServerOption{
 		grpc.UnaryInterceptor(
 			grpc_auth.UnaryServerInterceptor(validateJWT([]byte(jwtSecret))),
-=======
-	opts := []grpc.ServerOption{
-		grpc.UnaryInterceptor(
-			grpc_auth.UnaryServerInterceptor(validateJWT([]byte("secret"))),
->>>>>>> cdec08f (merged upstream changes)
 		),
 	}
 
@@ -106,17 +78,15 @@ func start() {
 	interview.RegisterInterviewServiceServer(grpcServer, api.New())
 	reflection.Register(grpcServer)
 
-<<<<<<< HEAD
 	log.Printf("Starting interview service at %s", address)
-=======
->>>>>>> cdec08f (merged upstream changes)
 	grpcServer.Serve(lis)
-
 }
 
+type authHeader string
+
 const (
-	authHeader = "authorization"
-	configPath = "./config/grpc.json"
+	auth_header = authHeader("authorization")
+	configPath  = "./config/grpc.json"
 )
 
 // validateJWT parses and validates a bearer jwt
@@ -132,36 +102,11 @@ func validateJWT(secret []byte) func(ctx context.Context) (context.Context, erro
 		claims, err := jwt.ValidateToken(token, secret)
 		if err != nil {
 			// log.Default().Println(err)
-			log.Debugf("Error %v", err)
+			log.WithError(err).Debugf("Token validation failed")
 			return nil, status.Errorf(codes.Unauthenticated, "invalid auth token: %v", err)
 		}
 
-		ctx = context.WithValue(ctx, authHeader, claims)
-
-		return ctx, nil
-	}
-}
-
-const authHeader = "authorization"
-
-// validateJWT parses and validates a bearer jwt
-//
-// TODO: move to own package (in ./internal/api/auth) using a constructor that privately sets the secret
-func validateJWT(secret []byte) func(ctx context.Context) (context.Context, error) {
-	return func(ctx context.Context) (context.Context, error) {
-		token, err := grpc_auth.AuthFromMD(ctx, "bearer")
-		if err != nil {
-			return nil, err
-		}
-
-		claims, err := jwt.ValidateToken(token, secret)
-		if err != nil {
-			//log.Default().Println(err)
-			log.WithError(err).Debug("Token Validation Failed")
-			return nil, status.Errorf(codes.Unauthenticated, "invalid auth token: %v", err)
-		}
-
-		ctx = context.WithValue(ctx, authHeader, claims)
+		ctx = context.WithValue(ctx, auth_header, claims)
 
 		return ctx, nil
 	}
--- service-go/go.mod
+++ service-go/go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.0
 	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
@@ -15,5 +17,4 @@ require (
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
